aaf0b382682d321e3a3c5f76191188003481db66
Javier Bolduc <javierbolduc51@gmail.com>
2026-02-04 09:46:53 -0500

    worked on the robot actions

--- 2025-2026/Code/src/robot_actions_ctrl.cpp
+++ 2025-2026/Code/src/robot_actions_ctrl.cpp
@@ -1,11 +1,9 @@
 //==============================================================================
 // ROBOT_ACTIONS_CTRL.CPP - Robot Actions Control
 //==============================================================================
-
 #include "main.h"
 #include "manette.h"
 #include "robot_actions_ctrl.h"
-
 
 //===================== BUTTON MAPPINGS =======================//
 // controller button mappings
@@ -24,55 +22,109 @@
 #define LS_ARM_TOP CRC_DIG_2            // Top arm position    (pin 23)
 #define LS_ARM_MIDDLE CRC_DIG_3         // Middle arm position (pin 24)
 #define LS_ARM_BOTTOM CRC_DIG_1         // Bottom arm position (pin 22)
+#define UP 0
+#define HALFWAY 1
+#define DOWN 2
 // grabbers
 #define LS_GRABBER_OPEN CRC_DIG_4       // Grabbers fully open (pin 25)
+#define OPEN 1
+#define CLOSED 0
 
-//=================== ELEVATOR STATE MACHINE ==================//
-void (*actionElevateur)() = initElevateur;
+
+//=========================== VARIABLES ========================//
+// elevator
 int8_t elevateurSpeed = 0;
-bool aimantAligne = false;
+bool topLimit = false;
+bool bottomLimit = false;
+// grabbers
+int8_t grabberSpeed;
+bool openLimit = false;
+bool closedLimit = false;
+int LastGrabberState = OPEN;
+// arm rotation
+int8_t armSpeed = 0;
+bool upwards = false;
+bool halfways = false;
+bool downwards = false;
+int LastarmtState = DOWN;
 
+
+//===================== FUNCTION POINTERS =======================//
+void (*actionElevateur)() = initElevateur;
+void (*actionGrabber)() = controlGrabber;
+void (*actionArm)() = controlArm;
+
+
+//======================== ELEVATOR CTRL =======================//
 void initElevateur() {
-    bool bottomLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_BOTTOM);  // true = pressed
+    bottomLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_BOTTOM);
     
-    if (bottomLimit) {
-        elevateurSpeed = 0;
-        actionElevateur = controlElevateur;
-    } else {
+    if (!bottomLimit) {
+        // move down until bottom limit is reached
         elevateurSpeed = -50;
+    } else {
+        elevateurSpeed = 0;
         actionElevateur = controlElevateur;
     }
 }
 
 void controlElevateur() {
-    bool topLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_TOP);        // true = pressed
-    bool bottomLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_BOTTOM);  // true = pressed
-    
-    // Both buttons pressed = hold position
+    topLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_TOP);
+    bottomLimit = CrcLib::GetDigitalInput(LS_ELEVATOR_BOTTOM);
+
     if (BTN_ELEVATOR_UP && BTN_ELEVATOR_DOWN) {
-        elevateurSpeed = 5;  // counter gravity
+        elevateurSpeed = 5; // slight up to hold position againt gravity
     }
-    // Up button only - can go up if NOT at top limit
     else if (BTN_ELEVATOR_UP && !topLimit) {
-        elevateurSpeed = 75;  // go up
+        elevateurSpeed = 75; // move up
     }
-    // Down button only - can go down if NOT at bottom limit
     else if (BTN_ELEVATOR_DOWN && !bottomLimit) {
-        elevateurSpeed = -50;  // go down
+        elevateurSpeed = -50; // move down
     }
-    // Nothing pressed or at limit
     else {
-        elevateurSpeed = 0;  // motor off
+        elevateurSpeed = 0; // notihn
     }
 }
 
+
 //===================== GRABBERS ====================//
-// literrylly open a closes with one or 2 limit switch
-// will be toggled with B button
-extern int8_t grabberSpeed;
+// literrylly open/closes with button B and one or 2 limit switch 
+void initGrabber() {
+    openLimit = CrcLib::GetDigitalInput(LS_GRABBER_OPEN);
+    //closedLimit = CrcLib::GetDigitalInput(LS_GRABBER_CLOSED);
+    
+    if (!openLimit) {
+        // open grabbers until open limit is reached
+        grabberSpeed = 50;
+    } else {
+        grabberSpeed = 0;
+        actionGrabber = controlGrabber;
+    }
+}
 
 void controlGrabber() {
-    // TODO: implement grabber control
+    openLimit = CrcLib::GetDigitalInput(LS_GRABBER_OPEN);
+    //closedLimit = CrcLib::GetDigitalInput(LS_GRABBER_CLOSED);
+    grabberSpeed = 50; 
+
+    if (BTN_GRABBER_TOGGLE) {
+        if (LastState == OPEN) {
+            // close grabbers
+            grabberSpeed = -50;
+            // fix for knowing when to stop the grabbers
+            // we don't have a closed limit switch
+            
+        }
+        else {
+            // open grabbers
+            if (!openLimit) {
+                grabberSpeed = 50;
+            } else {
+                grabberSpeed = 0;
+                LastGrabberState = OPEN;
+            }
+        }
+    }
 }
 
 
@@ -82,10 +134,34 @@ void controlGrabber() {
 // X for the arm to go to the middle position
 // A for the arm to go to the bottom position
 
-// note: for now only 2 limit switches are used (top and bottom)
-// and a magnet sencor thigy for the middle position
-extern int8_t armSpeed;
+void initArm() {
+    downwards = CrcLib::GetDigitalInput(LS_ARM_BOTTOM);
+    if (!downwards) {
+        // move down until bottom limit is reached
+        armSpeed = -50;
+    } else {
+        armSpeed = 0;
+        actionArm = controlArm;
+    }
+}
 
 void controlArm() {
-    // TODO: implement arm control
+    upwards = CrcLib::GetDigitalInput(LS_ARM_TOP);
+    halfways = CrcLib::GetDigitalInput(LS_ARM_MIDDLE);
+    downwards = CrcLib::GetDigitalInput(LS_ARM_BOTTOM);
+    
+    if (BTN_ARM_TOP) {
+        // move arm down until bottom limit is reached
+        armSpeed = -50;
+    } else if (BTN_ARM_MIDDLE) {
+        // move arm up until middle limit is reached
+        armSpeed = 50;
+    } else if (BTN_ARM_BOTTOM) {
+        if (!downwards) {
+            armSpeed = -50;
+        } else {
+            armSpeed = 0;
+        }
+    } else {
+    }
 }
